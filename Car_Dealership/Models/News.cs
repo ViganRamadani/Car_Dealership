b2270ab5c859709d25360c722d1fe2c38b443d0a
Vigan Ramadani <vigancseubt@hotmail.com>
2021-05-09 11:44:15 +0200

    Create News MongoDB

---
+++ Car_Dealership/Models/News.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Car_Dealership.Models
+{
+    public class News
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+    }
+}
