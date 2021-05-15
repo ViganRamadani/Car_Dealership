daa869b28e67b7f8a997db70b119701be877fced
Vigan Ramadani <vigancseubt@hotmail.com>
2021-05-15 19:05:32 +0200

    NodeJs Init and ExpressJs config MongoDB (Changing the implementation from ASP.NET and MongoDB)

---
+++ Car_Dealership/NodeServer/Controller/index.js
@@ -0,0 +1,26 @@
+﻿const express = require('express')
+const router = express.Router()
+const News = require('../Model/News');
+const multer = require('multer');
+const uploads = multer({ dest: 'uploads/' });
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, crypto.randomBytes(16).toString("hex") + ".jpg") //Appending extension
+    }
+})
+var upload = multer({ storage: storage });
+
+router.post('/createNews', (req, res) => {
+    News.create(req.body)
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(err => {
+            res.send("Some error occurred.");
+            console.log(err);
+        });
+});
+
+module.exports = router;
---
+++ Car_Dealership/NodeServer/Model/News.js
@@ -0,0 +1,15 @@
+﻿const mongoose = require('mongoose');
+
+let NewsSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    img_path: {type:String, required:true}
+},
+    {
+        collection: 'News'
+    },
+    {
+        timestamps: true
+    })
+var collectionName = 'News'
+module.exports = mongoose.model('News', NewsSchema, collectionName)
