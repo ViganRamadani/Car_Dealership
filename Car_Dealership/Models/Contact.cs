00b818e54346885a835cdb2236f6de746b0718ba
EdonaGjergji <eg45271@ubt-uni.net>
2021-06-23 11:54:10 +0200

    UI Design

---
+++ Car_Dealership/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Car_Dealership.Models
+{
+    public class Contact
+    {
+    }
+}
